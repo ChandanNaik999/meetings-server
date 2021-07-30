7f148a286d5ca774e6a5b3c832919d06b62a561f
ChandanNaik999 <chandannaik999@gmail.com>
2021-07-30 16:48:25 +0530

    Add Feature: excuse from meeting

--- controllers/meetings.js
+++ controllers/meetings.js
@@ -1,5 +1,6 @@
 const mongoose = require( 'mongoose' );
 const Meeting = mongoose.model( 'Meeting' );
+const User = mongoose.model( 'User' );
 
 const filterMeetings = async ( req, res, next ) => {
     let period = req.query.period;
@@ -100,7 +101,95 @@ const addAMeeting = async ( req, res, next ) => {
     }
 };
 
+const editMeeting = async ( req, res, next ) => {
+    let meetingId = req.params.id;
+    let action = req.query.action;
+
+    switch( action ){
+        case 'add_attendee':
+            let email = req.query.email;
+
+            if( !email ){
+                const error = new Error( 'Incorrect query parameter: email' );
+                error.status = 400;
+                return next( error );
+            }
+            try {
+                const user = await User.findOne( { email } ).select( { email:true, _id:true } );
+                
+                if( !user ){
+                    const error = new Error( 'User to be added does not exist' );
+                    error.status = 400;
+                    return next( error );
+                }
+                
+                let attendee = user.toObject();
+                attendee = {
+                    email : user.email,
+                    userId : user._id
+                }
+
+                const meeting = await Meeting.findByIdAndUpdate( 
+                    meetingId,
+                    {
+                        $addToSet: {
+                            attendees : attendee
+                        },
+                    },
+                );
+
+                if( !meeting ){
+                    const error = new Error( 'Meeting does not exist' );
+                    error.status = 400;
+                    return next( error );
+                }
+
+                res.status( 200 ).json({
+                    message: 'success',
+                    data: meeting
+                });
+            } catch( error ) {
+                return next( error );
+            }
+            break;
+        case 'remove_attendee':
+            console.log(res.locals.claims.email, meetingId);
+            try{
+                const meeting = await Meeting.findByIdAndUpdate( 
+                    meetingId,
+                    {
+                        $pull: {
+                            attendees : {
+                                email: res.locals.claims.email,
+                            }
+                        },
+                    },
+                );
+    
+                if( !meeting ){
+                    const error = new Error( 'Meeting does not exist' );
+                    error.status = 400;
+                    return next( error );
+                }
+    
+                res.status( 200 ).json({
+                    message: 'success',
+                    data: meeting
+                });
+            } catch( error ) {
+                return next( error );
+            }
+            break;
+        default:
+            const error = new Error( 'Incorrect query parameters: action' );
+            error.status = 400;
+            return next( error );
+    }
+    
+};
+
 module.exports = {
     filterMeetings,
     addAMeeting,
+    editMeeting,
 };
--- routes/meetings.js
+++ routes/meetings.js
@@ -1,10 +1,11 @@
 const express = require( 'express' );
-const { filterMeetings, addAMeeting } = require( '../controllers/meetings' );
+const { filterMeetings, addAMeeting, editMeeting } = require( '../controllers/meetings' );
 const { authenticate, authorize } = require( '../utils/auth' );
 
 const router = express.Router();
 
 router.post( '/', authenticate, authorize( 'general' ), addAMeeting );
 router.get( '/', authenticate, authorize( 'general' ), filterMeetings );
+router.patch( '/:id', authenticate, authorize( 'general' ), editMeeting );
 
 module.exports = router;
